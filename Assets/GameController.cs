d00ff4cb21adfdca38edf6a6f23833cba27c4bd0
st37071556@icloud.com <st37071556@icloud.com>
2022-09-10 16:26:34 +0900

    first

---
+++ Assets/GameController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    //
+    [SerializeField] GameObject _stonePrefab;
+    //石の実体インスタンス
+    GameObject[,] _blocks = new GameObject[8,8];
+    StoneController[,] _stones = new StoneController[8, 8];
+    // Start is called before the first frame update
+    void Start()
+    {
+        //初期設定
+        for(int i =0;i < 8;i++)
+        {
+            for (int j = 0; j < 8;j++)
+            {
+                GameObject newobject = Instantiate<GameObject>(_stonePrefab);
+                StoneController newController = newobject.GetComponent<StoneController>();
+                newobject.transform.localPosition = new Vector3(j, 0, i);
+                _blocks[i, j] = newobject;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
